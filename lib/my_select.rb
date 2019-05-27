d1c83b78a70982c50087eb8e82a898fad75cabcc
Amanda King <amapickell@gmail.com>
2019-05-27 21:42:34 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,11 @@
 def my_select(collection)
   i = 0
-  
+  selection = []
   while i < collection.size
-    
+    if yield collection[i] == true
+      selection << collection[i]
+    end
     i += 1
   end
-  
+  selection
 end
